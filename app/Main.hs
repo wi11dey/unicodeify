3f2b2e12bbb86b04105beb0cecac1e3b2076d4b8
Will Dey <wdey@palantir.com>
2026-08-20 12:12:54 +0100

    more

--- app/Main.hs
+++ app/Main.hs
@@ -17,39 +17,40 @@ import Language.Haskell.Tokens
 import Utils
 
 data Options = Options
-  { dryRun :: Bool
-  , paths :: NonEmpty FilePath
+  { dryRun ∷ Bool
+  , paths ∷ NonEmpty FilePath
   }
   deriving Show
 
-description :: InfoMod a
+description ∷ InfoMod a
 description = fullDesc <> progDesc "Enforce UnicodeSyntax usage in Haskell source. By default, runs on the Cabal project in the current working directory."
 
-options :: Parser Options
+options ∷ Parser Options
 options = Options <$>
   switch (long "dry-run" <> help "Fail with stderr if there is non-Unicode syntax") <*>
   (maybe (singleton ".") id . nonEmpty
     <$> many (argument str (metavar "PATHS..." <> help "Files or directories of Cabal projects to process")))
 
-filesFromCabal :: FilePath -> IO [([Extension], FilePath)]
+filesFromCabal ∷ FilePath → IO [([Extension], FilePath)]
 filesFromCabal cabalFile = do
-  package <- packageDescription <$> readGenericPackageDescription silent cabalFile
+  package ← packageDescription <$> readGenericPackageDescription silent cabalFile
   packageFiles ← listPackageSources silent "." package knownSuffixHandlers
+  putStrLn $ show packageFiles
   return $ (enabledPackageExtensions package,) <$> packageFiles
 
-main :: IO ()
+main ∷ IO ()
 main = do
-  Options {..} <- execParser $ info (options <**> helper) description
-  files <- concat <$> forM paths \path -> do
-    isFile      <- doesFileExist      path
-    isDirectory <- doesDirectoryExist path
+  Options {..} ← execParser $ info (options <**> helper) description
+  files ← concat <$> forM paths \path → do
+    isFile      ← doesFileExist      path
+    isDirectory ← doesDirectoryExist path
     if ".cabal" `isSuffixOf` path
     then if not isFile then do
       hPutStrLn stderr $ path ++ " is not a Cabal file as was expected"
       return []
     else filesFromCabal path
     else if isDirectory then do
-      findPackageDesc path >>= flip either filesFromCabal \exception -> do
+      findPackageDesc path >>= flip either filesFromCabal \exception → do
         hPutStrLn stderr $ "Couldn't find Cabal file in project " ++ path ++ ": " ++ exceptionMessage exception
         return []
     else if isFile then do
@@ -57,7 +58,7 @@ main = do
     else do
       hPutStrLn stderr $ path ++ " does not exist"
       return []
-  replacements <- concat <$> (
+  replacements ← concat <$> (
     forM files $
     uncurry $
     findReplaceableSpans unicodeSyntax)
